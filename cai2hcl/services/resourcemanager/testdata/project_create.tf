853a544fe0a93f3df96cb9acb600fe49a089dc59
Amir Romashkin <romashkin@google.com>
2023-12-15 14:26:02 +0000

    Add unit test for multiple projects in one batch

--- cai2hcl/services/resourcemanager/testdata/project_create.tf
+++ cai2hcl/services/resourcemanager/testdata/project_create.tf
@@ -1,5 +1,5 @@
-resource "google_project" "example-project" {
-  billing_account = "example-account"
+resource "google_project" "example-project-1" {
+  billing_account = "example-account-1"
   folder_id       = "456"
 
   labels = {
@@ -7,5 +7,17 @@ resource "google_project" "example-project" {
   }
 
   name       = "My Project"
-  project_id = "example-project"
+  project_id = "example-project-1"
+}
+
+resource "google_project" "example-project-2" {
+  billing_account = "example-account-2"
+  folder_id       = "456"
+
+  labels = {
+    project-label-key-a = "project-label-val-a"
+  }
+
+  name       = "My Project 2"
+  project_id = "example-project-2"
 }
